40b3f48f5f52d9126240cd439a534dece8c3036d
Sungmo <km700274@gmail.com>
2026-04-23 15:11:37 +0900

    섭씨화씨 프로그램

---
+++ 시험공부용.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int C;
+    double f;
+
+    printf("섭씨온도를 입력하세요 > ");
+    scanf("%d", &C);
+
+    f = 9.0 / 5 * c + 32;
+
+    printf("섭씨 %d도는 화씨 %f도입니다.Wn",c,f);
+
+    return 0;
+}
